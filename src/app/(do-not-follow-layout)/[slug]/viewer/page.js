faae78cff2c0dd3e1f6ca85e6b5df32113c979d4
bhavik-wts <bhavik.d@wingsts.com>
2025-05-01 13:53:35 +0530

    Page udpate

--- src/app/(do-not-follow-layout)/[slug]/viewer/page.js
+++ src/app/(do-not-follow-layout)/[slug]/viewer/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { useRef, useState, useEffect } from "react";
 import ModelViewer3d from "@/components/pages/viewer/ModelViewer3d";
 import { useParams, useRouter } from "next/navigation";
