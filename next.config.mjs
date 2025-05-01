23969639e7b316bf6c5533aa0b639b5e93fcbec9
bhavik-wts <bhavik.d@wingsts.com>
2025-05-01 13:57:54 +0530

    Webpack udpate

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,16 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-      })
-    );
-    return config;
-  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.plugins.push(
+  //     new webpack.ProvidePlugin({
+  //       $: "jquery",
+  //       jQuery: "jquery",
+  //       "window.jQuery": "jquery",
+  //     })
+  //   );
+  //   return config;
+  // },
 };
 
 export default nextConfig;
